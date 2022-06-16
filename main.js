97c2d3fa6943ba5c10147818f0dc7902b971e984
Luca <vintiluca@gmail.com>
2022-06-16 16:45:21 +0200

    prag als center der Karte bestimmt

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+let center = {
+    lat: 50.088611,
+    lng: 14.421389,
+    title: "Center (Prag)"
+};
+
 let startlayer = L.tileLayer.provider("Esri.WorldImagery")
 
 let map = L.map("map", {
+    center: [center.lat, center.lng],
     zoom: 2,
     layers: [
         startlayer
