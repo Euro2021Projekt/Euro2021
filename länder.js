d29dcdcb7fc5b0a380fd243315e328e88c582de4
Joel <joel1.robert@gmail.com>
2022-06-26 19:25:06 +0200

    länder informationen

--- länder.js
+++ länder.js
@@ -1,190 +1,190 @@
 const LÄNDER = [{
     "name": "Belgien",
-    "info1": "Kapitän: ",
+    "info1": "Trainer: Roberto Martinez  ",
     "info2": "EM-Teilnahmen: 6",
     "image": "icons/header.jpg",
     "lat": 50.843333,
     "lng": 4.363056
 },{
     "name": "Italien",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Tainer: Roberto Mancini ",
+    "info2": "EM-Teilnahmen:10 ",
     "image": "icons/länder/land2.jpg",
     "lat":41.9028 ,
     "lng": 12.4964
 },
 {
     "name": "Polen",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Paulo Sousa  ",
+    "info2": "EM-Teilnahmen: 4",
     "image": "icons/länder/land2.jpg",
     "lat": 52.2297,
     "lng": 21.0122
 },
 {
     "name": "Russland",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Stanislaw Tschertschessow ",
+    "info2": "EM-Teilnahmen: 11 ",
     "image": "icons/länder/land2.jpg",
     "lat":55.7558 ,
     "lng": 37.6173
 },
 {
     "name": "Ukraine",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Andrij Schewtschenko ",
+    "info2": "EM-Teilnahmen: 3 ",
     "image": "icons/länder/land2.jpg",
     "lat":50.4501 ,
     "lng": 30.5234
 },
 {
     "name": "Spanien",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Luis Enrique  ",
+    "info2": "EM-Teilnahmen: 11 ",
     "image": "icons/länder/land2.jpg",
     "lat":40.4168 ,
     "lng": -3.7038
 },
 {
     "name": "England",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Gareth Southgate ",
+    "info2": "EM-Teilnahmen:10 ",
     "image": "icons/länder/land2.jpg",
     "lat":51.5072 ,
     "lng": -0.1276
 },
 {
     "name": "Frankreich",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Didier Deschamps ",
+    "info2": "EM-Teilnahmen: 10 ",
     "image": "icons/länder/land2.jpg",
     "lat": 48.8566,
     "lng": 2.3522
 },
 {
     "name": "Tschechien",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Jaroslav Silhavy ",
+    "info2": "EM-Teilnahmen: 7 ",
     "image": "icons/länder/land2.jpg",
     "lat":50.0755 ,
     "lng":  14.4378
 },
 {
     "name": "Türkei",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Senol Günes ",
+    "info2": "EM-Teilnahmen: 5 ",
     "image": "icons/länder/land2.jpg",
     "lat": 39.9334 ,
     "lng": 32.8597
 },
 {
     "name": "Finnland",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Markku Kanerva ",
+    "info2": "EM-Teilnahmen: 1",
     "image": "icons/länder/land2.jpg",
     "lat":64.1466 ,
     "lng": -21.9426
 },
 {
     "name": "Schweden",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Janne Andersson ",
+    "info2": "EM-Teilnahmen: 7",
     "image": "icons/länder/land2.jpg",
     "lat": 59.3293,
     "lng": 18.0686
 },
 {
     "name": "Deutschland",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Joachim Löw ",
+    "info2": "EM-Teilnahmen: 13 ",
     "image": "icons/länder/land2.jpg",
     "lat": 52.5200,
     "lng": 13.4050
 },
 {
     "name": "Kroatien",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Zlatko Dalic ",
+    "info2": "EM-Teilnahmen: 5 ",
     "image": "icons/länder/land2.jpg",
     "lat":45.8150 ,
     "lng":  15.9819
 },
 {
     "name": "Niederlande",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Frank de Boer ",
+    "info2": "EM-Teilnahmen: 10",
     "image": "icons/länder/land2.jpg",
     "lat":52.3676 ,
     "lng": 4.9041
 },
 {
     "name": "Österreich",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Franco Foda ",
+    "info2": "EM-Teilnahmen: 3 ",
     "image": "icons/länder/land2.jpg",
     "lat": 48.2082,
     "lng": 16.3738
 },
 {
     "name": "Portugal",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Fernando Santos ",
+    "info2": "EM-Teilnahmen: 8 ",
     "image": "icons/länder/land2.jpg",
     "lat": 38.7223,
     "lng": -9.1393
 },
 {
     "name": "Dänemark",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Age Hareide ",
+    "info2": "EM-Teilnahmen: 9 ",
     "image": "icons/länder/land2.jpg",
     "lat": 55.6761,
     "lng": 12.5683
 },
 {
     "name": "Schweiz",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Vladimir Petkovic ",
+    "info2": "EM-Teilnahmen: 6 ",
     "image": "icons/länder/land2.jpg",
     "lat": 46.9480,
     "lng": 7.4474
 },
 {
     "name": "Wales",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Robert Page  ",
+    "info2": "EM-Teilnahmen: 2",
     "image": "icons/länder/land2.jpg",
     "lat": 51.4837,
     "lng": -3.1681
 },
 {
     "name": "Nordmazedonien",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Igor Angelovski ",
+    "info2": "EM-Teilnahmen: 1 ",
     "image": "icons/länder/land2.jpg",
     "lat": 41.9981,
     "lng": 21.4254
 },
 {
     "name": "Ungarn",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Marco Rossi  ",
+    "info2": "EM-Teilnahmen: 4  ",
     "image": "icons/länder/land2.jpg",
     "lat": 47.4979,
     "lng": 19.0402
 },
 {
     "name": "Slowakei",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Stefan Tarkovic  ",
+    "info2": "EM-Teilnahmen: 2 ",
     "image": "icons/länder/land2.jpg",
     "lat": 48.1486,
     "lng": 17.1077
 },
 {
     "name": "Schottland",
-    "info1": "Kapitän: ",
-    "info2": "EM-Teilnahmen: ",
+    "info1": "Trainer: Steve Clark ",
+    "info2": "EM-Teilnahmen: 3 ",
     "image": "icons/länder/land2.jpg",
     "lat": 55.9533,
     "lng": -3.1883
