a156e1aba5c531235c9a2c06d8aab051b52d436f
Joel <joel1.robert@gmail.com>
2022-06-26 15:34:33 +0200

    länder anfang

--- länder.js
+++ länder.js
@@ -1,19 +1,16 @@
 const Länder = [{
-    "name": "Lane Cove Hut",
-    "info": "Mattresses, Toilets - non-flush, Water from tap - not treated, boil before use, Water supply",
+    "name": "Belgien",
+    "info1": "Kapitän: ",
+    "info2": "EM-Teilnahmen: 6",
     "image": "icons/header.jpg",
-    "link": "https://www.doc.govt.nz/link/08cd5aa2d643478db1fe37a89a5c4bac.aspx",
-    "lat": -35.01224,
-    "lng": 173.73094
-}, {
-    "name": "Trains Hut",
-    "info": "Heating, Mattresses, Toilets - non-flush, Water from tap - not treated, boil before use, Water supply",
-    "region": "Manawatu/Whanganui, Wait\u014dtara Conservation Area",
-    "image": "https://www.doc.govt.nz/thumbs/large/link/020a8d5255c94dbe849fe75c9bab8fbf.jpg",
-    "link": "https://www.doc.govt.nz/link/c3398bb13fc94380a5bf3e5ef6fb13dc.aspx",
-    "bookable": false,
-    "open": true,
-    "lat": -39.47955,
-    "lng": 174.73017
+    "lat": 50.843333,
+    "lng": 4.363056
+},{
+    "name": "Belgien",
+    "info1": "Kapitän: ",
+    "info2": "EM-Teilnahmen: 6",
+    "image": "icons/header.jpg",
+    "lat": 50.843333,
+    "lng": 4.363056
 },
 ]
