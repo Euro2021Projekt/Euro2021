14115d11e68e6391877d56cc7c611efb0c13415f
Richard Matalewski <richard.matalewski@student.uibk.ac.at>
2022-06-27 17:26:05 +0200

    alle circle marker der größe nach angepasst

--- Statistik.js
+++ Statistik.js
@@ -16,21 +16,16 @@ L.circleMarker([41.9028, 12.4964],{
     .bindPopup("<h1>Italien</h1> <h1>1. Platz</h1>");
 
  L.circleMarker([40.4168, -3.7038],{
-    icon: L.icon({
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+    radius: 20,
+    color: "blue"
 }).addTo(map)
-    .bindPopup("Spanien");
+    .bindPopup("<h1>Spanien</h1> <h1>Gruppenzweite</h1>");
 
  L.circleMarker([55.6761, 12.5683],{
-    icon: L.icon({
-        
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+    radius: 20,
+    color: "blue"
 }).addTo(map)
-    .bindPopup("Dänemark");
+    .bindPopup("<h1>Dänemark</h1> <h1>Gruppenzweite</h1>");
 
 L.circleMarker([51.5072, -0.1276],{
     radius: 40,
@@ -39,183 +34,124 @@ L.circleMarker([51.5072, -0.1276],{
     .bindPopup("<h1>England</h1> <h1>2. Platz</h1>");
 
  L.circleMarker([50.8476, 4.3572],{
-    icon: L.icon({
-        
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+ radius: 30,
+ color: "blue"
 }).addTo(map)
-    .bindPopup(" <h2>Belgien</h1> <h1>51 Spiele gespielt</h2>");
+    .bindPopup("<h2>Belgien</h1> <h1>Gruppensieger</h1>");
 
  L.circleMarker([52.3676, 4.9041],{
-    icon: L.icon({
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+    radius: 30,
+    color: "blue"
 }).addTo(map)
-    .bindPopup("Niederlande");
+    .bindPopup("<h1>Niederlande</h1> <h1>Gruppensieger</h1>");
 
  L.circleMarker([46.9480, 7.4474],{
-    icon: L.icon({
-        
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+    radius: 10,
+    color: "blue"
 }).addTo(map)
-    .bindPopup("Schweiz");
+    .bindPopup("<h1>Schweiz</h1> <h1>Beste Gruppendritte</h1>");
 
  L.circleMarker([38.7223, -9.1393],{
-    icon: L.icon({
-       
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+    radius: 10,
+    color: "blue"
 }).addTo(map)
-    .bindPopup("Portugal");
+    .bindPopup("<h1>Portugal</h1> <h1>Beste Gruppendritte</h1>");
 
 L.circleMarker([48.8566, 2.3522],{
-    icon: L.icon({
-        
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+    radius: 30,
+    color: "blue"
 }).addTo(map)
-    .bindPopup("France");
+    .bindPopup("<h2>Frankreich</h1> <h1>Gruppensieger</h1>");
 
 L.circleMarker([45.8150, 15.9819],{
-    icon: L.icon({
-        
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+  radius: 20,
+  color: "blue"
 }).addTo(map)
-    .bindPopup("Kroatien");
+    .bindPopup("<h1>Kroatien</h1> <h1>Gruppenzweite</h1>");
 
 L.circleMarker([50.4501, 30.5234],{
-    icon: L.icon({
-       
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+   radius: 10,
+   color: "blue"
 }).addTo(map)
-    .bindPopup("Ukraine");
+    .bindPopup("<h1>Ukraine</h1> <h1>Beste Gruppendritte</h1>");
 
 L.circleMarker([52.5200, 13.4050],{
-    icon: L.icon({
-        
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+    radius: 20,
+    color: "blue"
 }).addTo(map)
-    .bindPopup("Deutschland");
+    .bindPopup("<h1>Deutschland</h1> <h1>Gruppenzweite</h1>");
 
 L.circleMarker([50.0755, 14.4378],{
-    icon: L.icon({
-       
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+  radius: 10,
+  color:"blue"
 }).addTo(map)
-    .bindPopup("Tschechische Republik");
+    .bindPopup("<h1>Tschechische Republik</h1> <h1>Beste Gruppendritte</h1>");
 
 L.circleMarker([48.2082, 16.3738],{
-    icon: L.icon({
-      
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+   radius: 20,
+   color: "blue"
 }).addTo(map)
-    .bindPopup("Österreich");
+    .bindPopup("<h1>Österreich</h1> <h1>Gruppenzweite</h1>");
 
 L.circleMarker([59.3293, 18.0686],{
-    icon: L.icon({
-       
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+    radius: 30,
+    color: "blue"
 }).addTo(map)
-    .bindPopup("Schweden");
+    .bindPopup("<h2>Schweden</h1> <h1>Gruppensieger</h1>");
 
  L.circleMarker([52.2297, 21.0122],{
-    icon: L.icon({
-        
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+   radius: 5,
+   color: "blue"
 }).addTo(map)
-    .bindPopup("Polen");
+    .bindPopup("<h2>Polen</h1> <h1>Gruppenvierter</h1>");
 
 L.circleMarker([51.4837, -3.1681],{
-    icon: L.icon({
-        
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+   radius: 20,
+   color: "blue"
 }).addTo(map)
-    .bindPopup("Wales");
+    .bindPopup("<h1>Wales</h1> <h1>Gruppenzweite</h1>");
 
  L.circleMarker([47.4979, 19.0402],{
-    icon: L.icon({
-        
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+   radius: 5,
+   color: "blue"
 }).addTo(map)
-    .bindPopup("Ungarn");
+    .bindPopup("<h2>Ungarn</h1> <h1>Gruppenvierter</h1>");
 
 L.cirlceMarker([55.7558, 37.6173],{
-    icon: L.icon({
-     
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+  radius: 5,
+  color: "blue"
 }).addTo(map)
-    .bindPopup("Russland");
+    .bindPopup("<h2>Russland</h1> <h1>Gruppenvierter</h1>");
 
 L.circleMarker([48.1486, 17.1077],{
-    icon: L.icon({
-       
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+  radius: 10,
+  color: "blue"
 }).addTo(map)
-    .bindPopup("Slowakei");
+    .bindPopup("<h1>Slowakei</h1> <h1>Beste Gruppendritte</h1>");
 
 L.circleMarker([41.9981, 21.4254],{
-    icon: L.icon({
-       
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+  radius: 5,
+  color: "blue"
 }).addTo(map)
-    .bindPopup("Nordmazedonien");
+    .bindPopup("<h2>Nordmazedonien</h1> <h1>Gruppenvierter</h1>");
 
 L.circleMarker([55.9533, -3.1883],{
-    icon: L.icon({
-       
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+    radius: 5,
+    color: "blue"
 }).addTo(map)
-    .bindPopup("Schottland");
+    .bindPopup("<h2>Schottland</h1> <h1>Gruppenvierter</h1>");
 
 L.circleMarker([39.9334, 32.8597],{
-    icon: L.icon({
-        
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+    radius: 5,
+    color: "blue"
 }).addTo(map)
-    .bindPopup("Türkei");
+    .bindPopup("<h2>Türkei</h1> <h1>Gruppenvierter</h1>");
 
 L.circleMarker([60.1699, 24.9384],{
-    icon: L.icon({
-        
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    })
+  radius: 10,
+  color: "blue"
 }).addTo(map)
-    .bindPopup("Finnland");
+    .bindPopup("<h1>Finnland</h1> <h1>Beste Gruppendritte</h1>");
 
 
 
